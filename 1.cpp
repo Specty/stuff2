38d3d49a50f951f4ac799e759e2460596e2e5e50
Specty <48211364+Specty@users.noreply.github.com>
2019-06-05 10:17:39 +0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double f(double x){
+	return -x * x + 2 * sin(x);
+}
+
+int main(){
+	double a, b, c, e;
+	cout<<"Enter [a;b]"<<endl;
+	cin>>a>>b;
+	cout<<"Enter e"<<endl;
+	cin>>e;
+	while (b - a > e){
+		c = (a + b) / 2;
+		if(f(b) * f(c) < 0)
+			a = c;
+		else
+			b = c;
+	}
+	cout<<(a + b)/2<<endl;
+	return 0;
+}
